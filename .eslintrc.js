afc36aee5e382e3a3f5037ea6ec4e1e363c84ae3
Tyrone Scafe <tscafejr@gmail.com>
2020-06-27 15:07:50 -0700

    Add eslint (@TScafeJR)

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'env': {
+        'mocha': true,
+        'es6': true
+    },
+    'extends': "@dukes-wine/eslint-config-dukes-wine",
+    'parser': 'babel-eslint',
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 8,
+    }
+};
